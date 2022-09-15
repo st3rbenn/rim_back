04ba1924117d8b59873957d41898a5dc9401d782
st3rbenn <94072263+st3rbenn@users.noreply.github.com>
2022-09-15 21:41:35 +0200

    Initial commit

---
+++ src/module.d.ts
@@ -0,0 +1,36 @@
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+declare module '*.mp4' {
+  const src: string;
+  export default src;
+}
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+declare module '*.json' {
+  const src: string;
+  export default src;
+}
+declare module '*.md' {
+  const src: string;
+  export default src;
+}
+declare module '*.css' {
+  const src: string;
+  export default src;
+}
+declare module '*.scss' {
+  const src: string;
+  export default src;
+}
