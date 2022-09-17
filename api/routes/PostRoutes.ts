4c1d702df88fcb6e12f62c5df151d8cf8d2ea1ef
st3rbenn <anthonincolas37000@gmail.com>
2022-09-17 16:38:55 +0200

    add post routes

---
+++ api/routes/PostRoutes.ts
@@ -0,0 +1,14 @@
+import express, { Express } from 'express';
+import { createPost, editPost, findAllPosts, findPostById, findPostsByUserId, deletePost } from '../controller/PostController.js';
+
+const PostRouter: Express = express();
+
+PostRouter
+  .get('/', findAllPosts)
+  .get('/user', findPostsByUserId)
+  .get('/:id', findPostById)
+  .post('/', createPost)
+  .put('/', editPost)
+  .delete('/', deletePost)
+
+  export default PostRouter;
